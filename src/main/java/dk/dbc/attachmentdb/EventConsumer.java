d518c1fa060724fc0b6143ab2471cd8fcacea372
Henrik Witt-Hansen <hwha@dbc.dk>
2018-09-14 10:57:53 +0200

    Use injection when ever possible
    Also adds the @Stateless annotation to allow use as EJB

--- src/main/java/dk/dbc/attachmentdb/EventConsumer.java
+++ src/main/java/dk/dbc/attachmentdb/EventConsumer.java
@@ -8,21 +8,33 @@ package dk.dbc.attachmentdb;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import dk.dbc.util.Timed;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 /**
  * This bean consumes events from the attachment queue and
  * ships them off to the SolrDocstore service for processing.
  */
+@Stateless
 public class EventConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(EventConsumer.class);
 
     @PersistenceContext(unitName = "attachmentdb_PU")
     EntityManager entityManager;
+
+    @Inject
+    @ConfigProperty(name = "CONSUMER_ID")
+    private String consumerId;
+
+    @Inject
+    SolrDocstoreConnector solrDocstoreConnector;
 
     /**
      * Consume next event from the attachment events queue.
@@ -47,13 +59,10 @@ public class EventConsumer {
      */
     @Timed
     public AttachmentDbEvent poll() {
-        String consumerId = System.getProperty("CONSUMER_ID");
-
         final List<AttachmentDbEvent> events = entityManager
                 .createNamedQuery(AttachmentDbEvent.REMOVE_EVENT_QUERY_NAME, AttachmentDbEvent.class)
                 .setParameter("consumerId", consumerId)
                 .getResultList();
-
         return events.isEmpty () ? null : events.get(0);
     }
 
@@ -64,8 +73,7 @@ public class EventConsumer {
     @Timed
     public void accept(AttachmentDbEvent event) throws AttachmentDbEventAcceptException {
         try {
-            SolrDocstoreConnectorFactory factory = new SolrDocstoreConnectorFactory ();
-            factory.getInstance ().dispatchEvent (event);
+            solrDocstoreConnector.dispatchEvent (event);
             LOGGER.info ("Accepted one event from the queue: {}", event.toString ());
         }
         catch( SolrDocstoreConnectorException solrDocstoreConnectorEvent) {
